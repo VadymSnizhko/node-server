a028262fd44e6b3bb831ea48fd55bba301af8d79
VadymSnizhko <snizhko@gmail.com>
2025-05-10 01:13:36 +0300

    Test get data with my Node-Server

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // щоб читати JSON з тіла POST
+
+let tableData = []; // сюди зберігаємо дані
+
+// POST — приймаємо дані з GAS
+app.post('/upload', (req, res) => {
+  tableData = req.body;
+  console.log('Отримано:', tableData);
+  res.json({ status: 'ok' });
+});
+
+// GET — відображаємо дані
+app.get('/', (req, res) => {
+  res.send(`
+    <h2>Отримані дані з таблиці:</h2>
+    <pre>${JSON.stringify(tableData, null, 2)}</pre>
+  `);
+});
+
+module.exports = app; // Для Vercel
